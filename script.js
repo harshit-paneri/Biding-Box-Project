eac45ca1070168dc80c352ca320faf9f9fc58add
Harshit Paneri <paneriharshit29@gmail.com>
2023-02-27 22:05:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+let four = document.getElementById('four');
+let five = document.getElementById('five');
+let six = document.getElementById('six');
+let seven = document.getElementById('seven');
+let alertt = document.getElementById('alert');
+let pass = document.getElementById('pass');
+let x = document.getElementById('x');
+let xx = document.getElementById('xx');
+let nt = document.getElementById('nt');
+let spade = document.getElementById('spade');
+let heart = document.getElementById('heart');
+let diamond = document.getElementById('diamond');
+let tree = document.getElementById('tree');
+let out_1 = document.getElementById('out_1');
+let out_2 = document.getElementById('out_2');
+let out_3 = document.getElementById('out_3');
+
+one.addEventListener('click', ()=>{
+    console.log("I am 1");
+})
+
+two.addEventListener('click', ()=>{
+    console.log("I am 2");
+})
+three.addEventListener('click', ()=>{
+    console.log("I am 3");
+})
+four.addEventListener('click', ()=>{
+    console.log("I am 4");
+})
+five.addEventListener('click', ()=>{
+    console.log("I am 5");
+})
+six.addEventListener('click', ()=>{
+    console.log("I am 6");
+})
+seven.addEventListener('click', ()=>{
+    console.log("I am 7");
+})
+alertt.addEventListener('click', ()=>{
+    console.log("I am alert");
+})
+pass.addEventListener('click', ()=>{
+    console.log("I am pass");
+})
+x.addEventListener('click', ()=>{
+    console.log("I am double the value");
+})
+xx.addEventListener('click', ()=>{
+    console.log("I am re double the value");
+})
+nt.addEventListener('click', ()=>{
+    console.log("I am NT");
+})
+heart.addEventListener('click', ()=>{
+    console.log("I am heart");
+})
+spade.addEventListener('click', ()=>{
+    console.log("I am spade");
+})
+diamond.addEventListener('click', ()=>{
+    console.log("I am diamond");
+})
+
+tree.addEventListener('click', ()=>{
+    console.log("I am tree");
+})
+
